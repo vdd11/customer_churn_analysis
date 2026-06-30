f011b585ccc10304e0e0bc78783578cbb173a748
vdd11 <79956819+vdd11@users.noreply.github.com>
2026-06-29 21:16:50 -0400

    initial commit

---
+++ scripts/load_data.py
@@ -0,0 +1,4 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine('')
---
+++ sql_queries/init_db.sql
@@ -0,0 +1,26 @@
+CREATE TABLE customers (
+customerID VARCHAR(255),
+gender VARCHAR(255),
+SeniorCitizen INTEGER,
+Partner VARCHAR(255),
+Dependents VARCHAR(255),
+tenure INTEGER,
+PhoneService VARCHAR(255),
+MultipleLines VARCHAR(255),
+InternetService VARCHAR(255),
+OnlineSecurity VARCHAR(255),
+OnlineBackup VARCHAR(255),
+DeviceProtection VARCHAR(255),
+TechSupport VARCHAR(255),
+StreamingTV VARCHAR(255),
+StreamingMovies VARCHAR(255),
+Contract VARCHAR(255),
+PaperlessBilling VARCHAR(255),
+PaymentMethod VARCHAR(255),
+MonthlyCharges DECIMAL(10,2),
+TotalCharges VARCHAR(255),
+Churn VARCHAR(255)
+);
+
+--SELECT *
+--FROM customers

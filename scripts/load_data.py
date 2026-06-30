73d5a726ee87733dc7dc3bddb05c14843dafef09
vdd11 <79956819+vdd11@users.noreply.github.com>
2026-06-29 22:47:38 -0400

    loaded data into customers table

--- scripts/load_data.py
+++ scripts/load_data.py
@@ -1,4 +1,27 @@
 import pandas as pd
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import os
+load_dotenv()
+table_name = input("What is your table's name? ")
+def run_load(table_name):
 
-engine = create_engine('')
+    db_path = f"postgresql+psycopg2://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    try:
+        engine = create_engine(db_path)
+        print("Successfully connected to the database.")
+
+        csv_path = 'csv_files/telco_churn.csv'
+        print(f"Reading file from {csv_path}...")
+        df = pd.read_csv(csv_path)
+        df.columns = df.columns.str.replace(' ', '_').str.lower()
+        with engine.begin() as connection:
+            print(f"Loading data into the {table_name} table...")
+            df.to_sql(table_name, con=connection, if_exists='replace', index=False)
+            print("Commit executed!")
+    except Exception as e:
+        print(f"An error has occured: {e}")
+    finally:
+        engine.dispose()
+if __name__ == "__main__":
+    run_load(table_name)
